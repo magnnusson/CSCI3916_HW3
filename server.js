660c91e70824be16ed94e7a8b25a89eb43f11e42
magnnusson <ahandukic@gmail.com>
2022-03-02 19:12:13 -0700

    fixed access control allow origin error

--- server.js
+++ server.js
@@ -40,10 +40,7 @@ function getJSONObjectForMovieRequirement(req) {
     return json;
 }
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
+
 
 
 router.post('/signup', function(req, res) {
