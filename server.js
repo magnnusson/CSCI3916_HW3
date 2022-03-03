26329da6585a9d3601fb890f43aa377ea7776433
magnnusson <ahandukic@gmail.com>
2022-03-02 20:56:16 -0700

    second commit

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function getJSONObjectForMovieRequirement(req) {
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
+       return res.status(400).json({success: false, msg: 'Please include both username and password to signup.'});
     } else {
         var user = new User();
         user.name = req.body.name;
@@ -59,7 +59,7 @@ router.post('/signup', function(req, res) {
                     return res.json(err);
             }
 
-            res.json({success: true, msg: 'Successfully created new user.'})
+            return res.status(200).json({success: true, msg: 'Successfully created new user.'});
         });
     }
 });
@@ -81,7 +81,7 @@ router.post('/signin', function (req, res) {
                 res.json ({success: true, token: 'JWT ' + token});
             }
             else {
-                res.status(401).send({success: false, msg: 'Authentication failed.'});
+                return res.status(401).send({success: false, msg: 'Authentication failed.'});
             }
         })
     })
