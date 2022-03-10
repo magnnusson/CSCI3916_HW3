bc457df5775b95a0b224826090ee4d5964b25bbd
magnnusson <ahandukic@gmail.com>
2022-03-09 20:56:06 -0700

    added movie schema

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ router.route('/movies')
         }
     )
     .get(function(req, res){ // in GET, we want to return all movies in the collection
-           res.send(Movie.find({}));
+           res.json(Movie.find({}));
         }
     )
     .post(function(req, res) { // in POST, we want to save a single movie
