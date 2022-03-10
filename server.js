a76e4e368af77ffb6fb92a26246f8fa584cada1b
magnnusson <ahandukic@gmail.com>
2022-03-10 14:20:41 -0700

    added movie schema

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ router.post('/signin', function (req, res) {
 
 router.route('/movies/:movieparameter')
     .get(authJwtController.isAuthenticated, function(req, res){
-        Movie.findOne({title: req.params}, function(err, movie){
+        Movie.findOne({title: req.params['0']}, function(err, movie){
             if(err) {
                return res.status(400).json(err);
             }
